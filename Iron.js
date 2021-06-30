59eacbff91808bbaa729a540d8861e80f6758b0d
Sarthak1920 <79746971+Sarthak1920@users.noreply.github.com>
2021-06-30 06:50:11 -0700

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,28 @@
+class Iron {
+    constructor(x, y) {
+      var options = {
+        'density':4,
+        'friction': 1.0,
+        'restitution':0.5
+      };
+      this.body = Bodies.rectangle(400, y, 50, 50, options);
+      this.width = 100;
+      this.height = 30;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+     
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('red')
+      fill('red')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
